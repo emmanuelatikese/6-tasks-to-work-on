f9ab036973118a5681008a77eaf14687344c9cee
emmanuelatikese <emmanuelatikese14@gmail.com>
2025-01-02 00:21:02 +0000

    adding

--- task1/GuessGame/Program.cs
+++ task1/GuessGame/Program.cs
@@ -2,53 +2,84 @@
 using System;
 
 class Program{
-    static void Main(){
-        console.WriteLine("Hello, Welcome to Guess Game !!!");
-        console.WriteLine("Can you guess right?");
-        console.WriteLine("You have only five tries to guess a number from 1 to 10");
 
+    static void IntroductionFunction(){
+        Console.WriteLine("Hello, Welcome to Guess Game !!!");
+        Console.WriteLine("Can you guess right?");
+        Console.WriteLine("You have only five tries to guess a number from 1 to 10");
+    }
 
-        int guessNum = Random.Next(1, 10);
+    static bool PlayAgain(){
+        Console.WriteLine("Do you want to play again (yes(Y/y) or no (N/n)?");
+        string response = Console.ReadLine() ?? string.Empty;
+        if (response == "y" || response == "yes" || response == "Y")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    static bool GameLogic(){
+        Random random = new Random();
+        int guessNum = random.Next(1, 11);
         for (int i = 1; i <= 5; i++)
         {
-            console.WriteLine("Your Guess(1-10):");
-            string guess = console.ReadLine();
+            Console.WriteLine("Your Guess(1-10):");
+            string guess = Console.ReadLine() ?? string.Empty;
 
-
-            if (int.TryParse(guess, out guessInt) && (guessInt > 0 && guessInt < 11)){
+            int guessInt;
+            if (int.TryParse(guess, out guessInt) && (guessInt > 0 && guessInt < 11))
+            {
                 if (guessInt == guessNum)
                 {
-                    console.WriteLine("You guess right!!!");
-                    break;
+                    Console.WriteLine("You guess right!!!");
+                    bool answer = PlayAgain();
+                    return answer;
                 }
                 else
                 {
                     if (i < 6)
                     {
-                        console.WriteLine("you guessed wrong !!! :(");
+                        Console.WriteLine("you guessed wrong !!! :(");
                         int triesLeft = 5 - i;
                         if (triesLeft > 1)
                         {
-                            console.WriteLine($"{triesLeft} tries left");
+                            Console.WriteLine($"{triesLeft} tries left");
                         }
                         else
                         {
-                            console.WriteLine("I guess this is your last try");
+                            Console.WriteLine("I guess this is your last shot");
                         }
                     }
                     else
                     {
-                        console.WriteLine("You guessed wrong :(");
+                        Console.WriteLine("You guessed wrong :(");
                     }
                 }
             }
-            else{
-                console.WriteLine("Type in numbers only from (1 - 10)");
+            else
+            {
+                Console.WriteLine("Type in numbers only from (1 - 10)");
             }
-
 
         }
 
+        Console.WriteLine($"The number is: {guessNum}");
+        bool reply = PlayAgain();
+        return reply;
+    }
+    static void Main(){
+        
+        bool start = true;
+
+        while (start){
+            IntroductionFunction();
+            bool res = GameLogic();
+            start = res;
+            if (start == false){
+                Console.WriteLine("Thanks for playing the game. Bye...");
+            }
+        }
 
     }
 }
--- task1/GuessGame/obj/Debug/net9.0/GuessGame.AssemblyInfo.cs
+++ task1/GuessGame/obj/Debug/net9.0/GuessGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c1a999d24cd4fcffb6276fd2aaeaf0b8766c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598121a12272d45558d1f0ad711f8348f061c86c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
